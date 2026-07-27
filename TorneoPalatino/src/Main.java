25d101107490597fb3b66815a771bc6d1d55c632
David Maximiliano Pablo Cuy <maximiliano090918@gmail.com>
2026-07-26 21:41:42 -0600

    Torneo Palatino con 5 equípos

---
+++ TorneoPalatino/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+
+        ControladorTorneo controlador = new ControladorTorneo();
+        controlador.iniciar();
+        
+    } 
+
+}
