b450e9118b21d386634324138d21d47305db2a4d
Joel Alejandro Chávez Pérez - 26888 <alejandrochavez3126a@gmail.com>
2026-08-02 12:07:29 -0600

    Se agrega el archivo Main.

---
+++ 05-WFWC/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ControladorMundial controlador = new ControladorMundial();
+        controlador.iniciar();
+        
+    }
+}
