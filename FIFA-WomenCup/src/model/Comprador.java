83c0ef8c8c8d35a914bcf01f808b976349b3aae1
MR-APEN <aleapen82@gmail.com>
2026-08-03 17:28:32 -0600

    add: atributos y metodos para Models

--- FIFA-WomenCup/src/model/Comprador.java
+++ FIFA-WomenCup/src/model/Comprador.java
@@ -1,5 +1,58 @@
 package model;
 
 public class Comprador {
-    
+    private String nombre;
+    private String email;
+    private int cantidadBoletos;
+    private double presupuestoMax;
+
+    public Comprador(String nombre, String email, int cantidadBoletos, double presupuestoMax) {
+        this.nombre = nombre;
+        this.email = email;
+        this.cantidadBoletos = cantidadBoletos;
+        this.presupuestoMax = presupuestoMax;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getCantidadBoletos() {
+        return cantidadBoletos;
+    }
+
+    public double getPresupuestoMax() {
+        return presupuestoMax;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCantidadBoletos(int cantidadBoletos) {
+        this.cantidadBoletos = cantidadBoletos;
+    }
+
+
+    public void setPresupuestoMax(double presupuestoMax) {
+        this.presupuestoMax = presupuestoMax;
+    }
+
+    @Override
+    public String toString() {
+        return "Comprador{" +
+                "nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", cantidadBoletos=" + cantidadBoletos +
+                ", presupuestoMax=" + presupuestoMax +
+                '}';
+    }
 }
--- FIFA-WomenCup/src/model/Ticket.java
+++ FIFA-WomenCup/src/model/Ticket.java
@@ -1,5 +1,60 @@
 package model;
+import java.util.Random;
 
 public class Ticket {
-    
+    private static final int RANGO_MIN = 1;
+    private static final int RANGO_MAX = 15000;
+    private static final Random RANDOM = new Random();
+
+    private int numeroTicket;
+    private int numeroA;
+    private int numeroB;
+    private boolean valid;
+    private Localidad localidadAsig;
+
+    public int randomNumbers(){
+        return RANGO_MIN + RANDOM.nextInt(RANGO_MAX);
+    }
+
+    public boolean validarRango() {
+        int limiteInferior = Math.min(numeroA, numeroB);
+        int limiteSuperior = Math.max(numeroA, numeroB);
+        valid = numeroTicket >= limiteInferior && numeroTicket <= limiteSuperior;
+        return valid;
+    }
+
+    public void generarNumeros() {
+        this.numeroTicket = randomNumbers();
+        this.numeroA = randomNumbers();
+        this.numeroB = randomNumbers();
+        this.valid = validarRango();
+    }
+
+    public Ticket() {
+        generarNumeros();
+    }
+
+    public int getNumeroTicket() {
+        return numeroTicket;
+    }
+
+    public int getNumeroA() {
+        return numeroA;
+    }
+
+    public int getNumeroB() {
+        return numeroB;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public Localidad getLocalidadAsig() {
+        return localidadAsig;
+    }
+
+    public void setLocalidadAsig(Localidad localidadAsig) {
+        this.localidadAsig = localidadAsig;
+    }
 }
