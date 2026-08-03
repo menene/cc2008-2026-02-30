8dc1dbb974062e6940b2871ca2b388e11eee9bda
RogeeerrrMen <men26786@uvg.edu.gt>
2026-08-03 10:10:20 -0600

    Clase Localidad

---
+++ Ejericio1_MunidalF27/src/Localidad.java
@@ -0,0 +1,33 @@
+public class Localidad{
+
+    private int numero;
+    private double precio;
+    private int capacidad;
+    private int vendidos;
+
+    public Localidad(int numero, double precio, int capacidad){
+
+        this.numero = numero;
+        this.precio = precio;
+        this.capacidad = capacidad;
+        this.vendidos = 0;
+    }
+
+    public boolean hayEspacio(){
+        return vendidos < capacidad;
+    }
+
+    public void venderBoletos(int cantidad){
+        vendidos += cantidad;
+    }
+
+    public int getDisponibles(){
+        return capacidad - vendidos;
+    }
+
+    public double getIngresos(){
+        return vendidos * precio;
+    }
+
+
+}
