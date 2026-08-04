7060c8cdd12508a103f55298dd4a9f49bf12b6d9
lau-sth-07 <laura.susethmm@gmail.com>
2026-08-03 22:10:09 -0600

    Localidad y Vista

--- WorldCup/src/Localidad.java
+++ WorldCup/src/Localidad.java
@@ -1,8 +1,74 @@
 public class Localidad {
-    int localidad;
-    int precioLocalidad;
-    int capacidadMaxima;
-    int boletosVendidos;
+    private int localidad;
+    private int precioLocalidad;
+    private int capacidadMaxima;
+    private int boletosVendidos;
+    
+
+    //vacio primero
+    public Localidad() {
+
+    }
+
+    public Localidad(int localidad, int precioLocalidad, int capacidadMaxima, int boletosVendidos) {
+        this.localidad = localidad;
+        this.precioLocalidad = precioLocalidad;
+        this.capacidadMaxima = capacidadMaxima;
+        this.boletosVendidos = boletosVendidos;
+    }
+
+    //localidad
+    public int getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(int localidad) {
+        this.localidad = localidad;
+    }
+
+    //precioLocalidad
+    public int getPrecioLocalidad() {
+        return precioLocalidad;
+    }
+
+    public void setPrecioLocalidad(int precioLocalidad) {
+        this.precioLocalidad = precioLocalidad;
+    }
+
+    //capacidadMaxima
+    public int capacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    //boletosVendidos
+    public int getBoletosVendidos() {
+        return boletosVendidos;
+    }
+
+    public void setBoletosVendidos(int boletosVendidos) {
+        this.boletosVendidos = boletosVendidos;
+    }
+
+
+    //verificar si hay espacio disponible
+    public boolean hayEspacio() {
+        return boletosVendidos < capacidadMaxima;
+        //true si hay menos boletos vendidos que capacidad
+        //sino, false
+    }    
+
+
+    //boletos disponibles
+    public int boletosDisponibles() {
+        return capacidadMaxima - boletosVendidos;
+        //los restantes
+    }
+
+
     
 
 }
