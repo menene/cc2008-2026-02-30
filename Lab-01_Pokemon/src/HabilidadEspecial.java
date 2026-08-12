276133cfa89b5e99d0a9caad457149f0c3448807
RogeeerrrMen <men26786@uvg.edu.gt>
2026-08-12 07:54:46 -0600

    Adicion de clases a utilizar y finalizacion de la clase HabilidadEspecial

---
+++ Lab-01_Pokemon/src/HabilidadEspecial.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class HabilidadEspecial{
+
+    private String nombre;
+    private int probabilidadActivacion;
+
+    public HabilidadEspecial(String nombre,int probabilidadActivacion){
+
+        this.nombre = nombre;
+        this.probabilidadActivacion = probabilidadActivacion;
+
+    }
+
+    public String getNombre(){
+
+        return nombre;
+    }
+
+    public int getProbabilidadActivacion(){
+
+        return probabilidadActivacion;
+    }
+
+    public boolean seActiva(){
+
+        Random random = new Random();
+        int numero = random.nextInt(101);
+        return numero <= probabilidadActivacion;
+    }
+
+    public int generarPotenciador(){
+
+        Random random = new Random();
+        return random.nextInt(26);
+        
+    }
+}
