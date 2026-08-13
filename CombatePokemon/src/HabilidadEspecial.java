5f7596236214d6db8f94c8e4568429dc5841fba2
lau-sth-07 <laura.susethmm@gmail.com>
2026-08-12 21:04:40 -0600

    metodos de HabilidadEspecial, Pokemon y Entrenador

--- CombatePokemon/src/HabilidadEspecial.java
+++ CombatePokemon/src/HabilidadEspecial.java
@@ -5,6 +5,50 @@ public class HabilidadEspecial {
     private int probabilidadActivacion;
     private Random random;
 
+    //constructor
+    public HabilidadEspecial(String nombre, int probabilidadActivacion) {   
+        this.nombre = nombre;
+        this.probabilidadActivacion = probabilidadActivacion;
+        this.random = new Random();
+    }
+
+    //G&S nombre
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //G&S probabilidad Activacion
+    public int getProbabilidadActivacion() {
+        return probabilidadActivacion;
+    }
+
+    public void setProbabilidadActivacion(int probabilidadActivacion) {
+        this.probabilidadActivacion = probabilidadActivacion;
+    }
+
+    //metodo intentar activacion generando numero aleatorio
+    public boolean intentarActivacion() {
+        //rango de 0 a 100, entonces 101 porque no se toma ese ultimo
+        int num1 = random.nextInt(101);
+
+        //veo si se activa su habilidad o no
+        if (num1 <= probabilidadActivacion){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //metodo generar potenciador
+    public int generarPotenciador() {
+        //rango de 0 a 25
+        int num2 = random.nextInt(26);
+        return num2; 
+    }
 
 
 }
