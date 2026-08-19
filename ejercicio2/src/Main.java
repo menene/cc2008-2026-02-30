c79a06391fdfb67a91d657cfc5f4ed2dc25a13a3
Joel Alejandro Chávez Pérez - 26888 <alejandrochavez3126a@gmail.com>
2026-08-18 22:26:07 -0600

    Se agrega el proyecto en parejas de atletismo UVG.

---
+++ ejercicio2/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ControladorCompetencia controlador = new ControladorCompetencia();
+        controlador.iniciar();
+    }
+}
