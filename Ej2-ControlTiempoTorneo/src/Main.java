7e7ab5517d8658932a50e7b85760f2a0d1af7360
Emily Menchu <menchu.lucila@gmail.com>
2026-08-18 20:44:47 -0600

    Se agregó lógica en las clases Atleta y Controlador, además del inicializador en el Main

--- Ej2-ControlTiempoTorneo/src/Main.java
+++ Ej2-ControlTiempoTorneo/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         
+        ControladorTorneo controlador = new ControladorTorneo();
+
+        controlador.mostraMenuAtletas();
     }
 }
