abf58a9b013b8bbf081fe08f6fe37ac25a31b6c0
Daniel Nájera <danielnajera@MacBook-Air-de-Daniel.local>
2026-08-19 18:26:21 -0600

    Los cambios que llevaba antes de fetchar lo de Emily

--- Ej2-ControlTiempoTorneo/src/Torneo.java
+++ Ej2-ControlTiempoTorneo/src/Torneo.java
@@ -1,3 +1,20 @@
 public class Torneo {
+    private Atleta[] atletas; 
+
+
+    public Torneo(Atleta[] atletas){
+
+        this.atletas = new Atleta[1];
+        atletas[0] = new Atleta("Juan", 17, 1, 10);
     
+    }
+    public Atleta getAtleta(int numeroParticipante){
+
+        return atletas[(numeroParticipante - 1)];
+    }
+    public String toString(){
+        return "waza";
+    }
+        
+
 }
