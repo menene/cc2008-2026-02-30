f6ff00c3c8ca1614533ae95e86cfed1c7d556135
David Pablo <maximiliano090918@gmail.com>
2026-08-26 02:04:26 -0600

    Ejercicio 3 Taller Automotriz  - David Maximiliano Pablo Cuy - 261341

---
+++ src/DatosInvalidosException.java
@@ -0,0 +1,6 @@
+public class DatosInvalidosException extends Exception {
+
+    public DatosInvalidosException(String mensaje) {
+        super(mensaje);
+    }
+}
---
+++ src/OrdenDuplicadaException.java
@@ -0,0 +1,6 @@
+public class OrdenDuplicadaException extends Exception {
+
+    public OrdenDuplicadaException(String mensaje) {
+        super(mensaje);
+    }
+}
