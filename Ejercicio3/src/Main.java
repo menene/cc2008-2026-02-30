7b237165567b82e8fb03f2a31ad05cd3943cec00
Pilar Ordoñez <mdpilaroh@gmail.com>
2026-08-25 23:30:25 -0600

    Arreglos Dinamicos ejercicio completo

---
+++ Ejercicio3/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Vista vista = new Vista();
+        Controlador controlador = new Controlador(vista);
+
+        controlador.iniciar();
+    }
+}
---
+++ Ejercicio3/src/OrdenServicio.java
@@ -0,0 +1,46 @@
+public class OrdenServicio {
+
+    private int numeroOrden;
+    private String nombrePropietario;
+    private String placa;
+    private String descripcionServicio;
+    private double costoEstimado;
+
+    public OrdenServicio(int numeroOrden, String nombrePropietario, String placa,
+                         String descripcionServicio, double costoEstimado) {
+
+        this.numeroOrden = numeroOrden;
+        this.nombrePropietario = nombrePropietario;
+        this.placa = placa;
+        this.descripcionServicio = descripcionServicio;
+        this.costoEstimado = costoEstimado;
+    }
+
+    public int getNumeroOrden() {
+        return numeroOrden;
+    }
+
+    public String getNombrePropietario() {
+        return nombrePropietario;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getDescripcionServicio() {
+        return descripcionServicio;
+    }
+
+    public double getCostoEstimado() {
+        return costoEstimado;
+    }
+
+    public void setDescripcionServicio(String descripcionServicio) {
+        this.descripcionServicio = descripcionServicio;
+    }
+
+    public void setCostoEstimado(double costoEstimado) {
+        this.costoEstimado = costoEstimado;
+    }
+}
